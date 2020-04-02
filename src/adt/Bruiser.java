5ad5370fbe63b7ca8dfd1634ff7d5b395892f3bd
AJ Natzic <42810761+ajnatzic@users.noreply.github.com>
2020-04-01 20:48:57 -0400

    docs: add more docs to bruiser class

--- src/adt/Bruiser.java
+++ src/adt/Bruiser.java
@@ -6,20 +6,30 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 /**
- * Tougher enemy type that will have more health. Represented by a more menacing png model
+ * Tougher enemy type that will have more health. Represented by a more menacing PNG model
  *
  */
 public class Bruiser extends Enemy {
+	
+	/** This variable will hold the PNG that will be applied to the Bruiser */
     private BufferedImage graphic;
+    
     /**
-     * Initializers for the Bruiser that will put the enemy at the beginning of the path
-     * @param position
+     * Initializer that puts the Bruiser at a specific position on the game map based 
+     * on position variable
+     * @param position	X and Y coordinates on the game map
      */
     public Bruiser(Point position) {
         super(position.x, position.y, 50, 5, 100, 2);
         initialize();
     }
-
+    
+    /**
+     * Initializer that puts the Bruiser at a specific position on the game map based 
+     * on x and y coordinates
+     * @param x	number of pixels on x axis
+     * @param y	number of pixels on y axis
+     */
     public Bruiser(int x, int y) {
         super(x, y, 50, 5, 100, 2);
         initialize();
